057b08be1054f8742b89544481f11f49f5e2251b
Ashraful352 <ashrafula7880@gmail.com>
2025-07-27 04:12:31 +0600

    task work add

---
+++ task.js
@@ -0,0 +1,14 @@
+var orangesprice = 1000;
+var applesprice = 700;
+var total = orangesprice - applesprice;
+
+console.log(total);
+
+var Mathematics = 70.00005;
+var Biology = 80.500;
+var Chemistry = 90;
+var Physics = 60.700;
+var Bangla  = 60.100;
+var total = (Mathematics + Biology + Chemistry + Physics + Bangla);
+var average = (total/5);
+console.log (average.toFixed(2));
