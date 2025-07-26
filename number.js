41f5e69154cbac23363bcd2f3ebb43e91b1071e5
Ashraful352 <ashrafula7880@gmail.com>
2025-07-27 03:14:20 +0600

    script number add

---
+++ number.js
@@ -0,0 +1,16 @@
+var orangeprice = 20;
+var appleprice = 40;
+var choclateprice = 0.5;
+var total = orangeprice + appleprice + choclateprice;
+
+// or we can use //
+
+var bananaprice = parseInt ("20");
+var waterprice = parseFloat ("30.8");
+var sum = bananaprice + waterprice;
+
+console.log(typeof waterprice);
+console.log(typeof bananaprice);
+
+console.log(sum);
+console.log(total);
