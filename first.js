1852416edb757edafc0135ae8d9132b46f6a6d3c
Ashraful352 <ashrafula7880@gmail.com>
2025-07-27 02:07:28 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,20 @@
+
+var age = 26;
+var money = 30;
+money = 60;
+var price = 677;
+var year = 1995;
+var name = ("ashraful");
+var passed = true;
+
+
+console.log(age);
+console.log(money);
+console.log(price);
+console.log(year);
+console.log(name);
+console.log(typeof name);
+console.log(typeof price);
+console.log(passed);
+console.log(typeof passed);
+
