ed7f910e17ff7ef207a8bd5abfbfd34903669fbe
Ashraful352 <ashrafula7880@gmail.com>
2025-07-27 22:00:42 +0600

    grating apply

---
+++ grating.js
@@ -0,0 +1,28 @@
+const result = 51;
+if(result<33){
+    console.log('Fail');
+}
+else if (result>80){
+    console.log('A+');
+}
+else if (result>70){
+    console.log('A');
+}
+else if (result>60){
+    console.log('B+');
+}
+else if (result>50){
+    console.log('B');
+}
+else if (result>40){
+    console.log('c');
+    
+}
+
+else{
+    console.log('pass')
+}
+
+
+     
+   
