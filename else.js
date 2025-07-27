88bccf76e0230b9c6caf2fb30d908b3c8b03135b
Ashraful352 <ashrafula7880@gmail.com>
2025-07-27 21:35:46 +0600

    else condition add

---
+++ else.js
@@ -0,0 +1,26 @@
+const age = 45;
+const price = 5000;
+if ( age<12){
+    console.log('give service free')
+}
+else if( age>60){
+    // 50% discount
+    const discount = price*50/100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else if ( age>40){
+    // 20% discount
+    const discount = price*20/100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else if (age>50){
+    //30% discount
+    const discount = price*30/100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else{
+    console.log(price);
+}
