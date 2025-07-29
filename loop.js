4b81558193f19ecc83958badc77d46a79e8ec841
Ashraful352 <ashrafula7880@gmail.com>
2025-07-29 19:55:11 +0600

    loop condition stor

---
+++ loop.js
@@ -0,0 +1,41 @@
+//const fruits = ['bannar', 'kmola', 'anarosh', 'jambora','ashraful']
+//for(const fruit of fruits){
+    //console.log(fruit);
+    //console.log('i want to ear')
+//}
+
+//let num = 1;
+//let sum = 0;
+//while(num<=20){
+   // console.log(num);
+   // sum = sum + num;
+    //console.log('sum :', sum);
+    ////num++;
+//}
+
+let num = 1;
+while ( num<=10){
+    console.log(num);
+    if(num % 3 === 0 )
+        console.log('even num :', num)
+    num++;
+}
+
+for (let i = 1; i<=5; i++){
+    console.log(i);
+}
+let sum = 0
+for (let i = 11; i<=20; i++){
+    sum = sum + i;
+    console.log(i);
+}
+console.log('total sum : ', sum);
+
+for ( let x = 15; x>=0; x--){
+    console.log(x);
+}
+let y = 20;
+while(y>=0){
+    console.log(y);
+    y--;
+}
